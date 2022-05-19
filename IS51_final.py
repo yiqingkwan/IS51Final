1a61604ba95423e9794bd2639cbc9c9c33130f8b
Natalie Kwan <natalie@swuser-129-8-194-169.swlan.csufresno.edu>
2022-05-19 16:39:13 -0700

    finish pseudocode

--- IS51_final.py
+++ IS51_final.py
@@ -4,3 +4,26 @@ of grades that are above the average grade of 83.25.
 The program will get the grades from a text file called "Final.txt" that contains 24 student grades.
 We will use three different functions to get the three different grades.
 """
+
+"""
+main()
+infile = open("filename", 'r') #open file and read the data 
+#assign information in text to variable named grades
+grades = [line.rstrip()]
+
+grades[i] = int(grades[i]) #change the items in list to integer for items from Final.txt
+grade_average = sum of total grades / number of grades in list #to get average grade
+
+#To find the grades that are above average
+for every grade in grades
+    if the grade is greater than grade_average
+    increment counter by 1
+
+#To find the percentage of grades above average grade
+set grades_above_average to: counter divided by total number of grades in the list, then * 100 
+to get the percentage number
+
+print total amount of grades
+print average grade
+print percentage of grades above average
+"""
