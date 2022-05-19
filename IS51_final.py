6cde875f2f978df9e03e17aeb71c4ed7bb5d4a86
Natalie Kwan <natalie@swuser-129-8-194-169.swlan.csufresno.edu>
2022-05-19 16:01:04 -0700

    finish structured english

---
+++ IS51_final.py
@@ -0,0 +1,6 @@
+"""
+The program is trying to display the number of grades, the average grade, and the percentage
+of grades that are above the average grade of 83.25. 
+The program will get the grades from a text file called "Final.txt" that contains 24 student grades.
+We will use three different functions to get the three different grades.
+"""
